b5afe21cc94949fa6f45d0874ca77bee010783d8
yarrobong <oneboardsholi@gmail.com>
2023-10-26 21:18:50 +0500

    Добавьте файлы проекта.

---
+++ ConsoleAp8/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите строку для проверки:");
+string str = Console.ReadLine();
+
+bool isValidOctalNumber = true;
+
+if (string.IsNullOrWhiteSpace(str))
+{
+    isValidOctalNumber = false;
+}
+else
+{
+    foreach (char digit in str)
+    {
+        if (digit < '0' || digit > '7')
+        {
+            isValidOctalNumber = false;
+            break;
+        }
+    }
+}
+
+if (isValidOctalNumber)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
